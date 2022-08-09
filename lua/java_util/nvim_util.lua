5ac8adbebaf4fae12d8cc3b48dbd38d007e25e05
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2022-08-09 18:24:22 +0200

    fix: remove nvim_util since it is no longer used

--- lua/java_util/nvim_util.lua
+++
@@ -1,8 +0,0 @@
-local nvim_util = {}
-
-function nvim_util.feedkeys(keys, opts)
-  local replaced = vim.api.nvim_replace_termcodes(keys, true, false, true)
-  vim.api.nvim_feedkeys(replaced, opts or "n", true)
-end
-
-return nvim_util
